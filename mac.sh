ae406c82f7bea127318306828eb9171a7c911fd9
NHT <huutai90909@gmail.com>
2025-07-31 23:40:05 +0700

    Create mac.sh

---
+++ mac.sh
@@ -0,0 +1,6 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install php && brew install node && brew install git && brew install composer && composer global require laravel/installer
